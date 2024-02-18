ded7882ad8220c7f9a0781301fdbe78dc8573d22
GaviniSumanth <sumanth_edu@outlook.com>
2024-02-18 18:09:01 +0000

    feat(devcontainer): auto-start app in devcontainer

--- app.R
+++ app.R
@@ -3,5 +3,6 @@ port <- commandArgs(trailingOnly = TRUE)
 port <- if (length(port) > 0 && !is.na(as.numeric(port))) as.numeric(port) else 3000
 shiny::runApp(
     appDir = getwd(),
-    port = port
+    host = "0.0.0.0",
+    port = port,
 )
